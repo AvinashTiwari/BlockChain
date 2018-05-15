b60a7d368f773bac10b1d50ed371a9c02683dd62
Avinash Tiwari <qwe123kids@gmail.com>
2018-05-14 19:31:43 -0600

    Wallet

--- sf-chain/dev-test.js
+++ sf-chain/dev-test.js
@@ -1,6 +1,14 @@
+/*
 const BlockChain = require('./blockchain');
 const bc = new BlockChain();
 
 for(let i=0; i<10; i++){
    console.log( bc.addBlock(`foo ${i}`).toString());
 }
+*/
+
+//Wallet
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+
+console.log(wallet.toString());
