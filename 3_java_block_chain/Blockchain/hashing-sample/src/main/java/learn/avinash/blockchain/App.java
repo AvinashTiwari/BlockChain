cd1946ccca720adde89d920abb8fb5f7f1f246ae
Avinash Tiwari <qwe123kids@gmail.com>
2019-04-03 21:42:31 -0600

    Block chain

---
+++ 3_java_block_chain/Blockchain/hashing-sample/src/main/java/learn/avinash/blockchain/App.java
@@ -0,0 +1,12 @@
+package learn.avinash.blockchain;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		//System.out.println(SHA256Helper.hash(SHA256Helper.hash("Hello world!")));
+		System.out.println(SHA256Helper.hash("Hello world!"));
+		System.out.println(SHA256Helper.hash("Hello world!"));
+		
+	}
+}
